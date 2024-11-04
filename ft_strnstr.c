d7f5d3955fc962fc3c9a0d7238b5391a9abfe762
Louis Girerd <lgirerd@z4r3p6.42lyon.fr>
2024-11-04 16:05:08 +0100

    finished strnstr

---
+++ ft_strnstr.c
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strnstr.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lgirerd <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/04 15:50:40 by lgirerd           #+#    #+#             */
+/*   Updated: 2024/11/04 16:04:32 by lgirerd          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+
+char	*ft_strnstr(const char *big, const char *little, size_t len)
+{
+	int	i;
+	int	j;
+
+	i = 0;
+	if (little[0] == '\0')
+		return (str);
+	while (big[i] != '\0' && i < len)
+	{
+		j = 0;
+		while (little[j] == big[i + j])
+		{
+			if (little[j + 1] == '\0')
+				return (big + i);
+			j++;
+		}
+		i++;
+	}
+	return ((void *)(0));
+}
