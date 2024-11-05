080f50ba98cbc9bb3aa3551157adbb4ea1c7c2a7
Louis Girerd <lgirerd@z4r3p6.42lyon.fr>
2024-11-05 15:01:50 +0100

    fixed strchr

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -1,27 +1,55 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strchr.c                                        :+:      :+:    :+:   */
+/*   ft_strtrim.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: lgirerd <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/11/04 14:15:56 by lgirerd           #+#    #+#             */
-/*   Updated: 2024/11/05 15:01:39 by lgirerd          ###   ########.fr       */
+/*   Created: 2024/11/05 14:39:47 by lgirerd           #+#    #+#             */
+/*   Updated: 2024/11/05 15:01:08 by lgirerd          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-char	*ft_strchr(const char *s, int c)
+#include <stdlib.h>
+
+int	ft_strstrchr(const char *s, const char *set)
+{
+	int	i;
+	int	j;
+
+	i = 0;
+	while (set[i])
+	{
+		j = 0;
+		while (s[i])
+		{
+			if (s[i] == c)
+				return (1);
+			j++;
+		}
+		i++;
+	}
+	return (0);
+}
+
+int	ft_strlen(const char *s)
 {
 	int	i;
 
 	i = 0;
 	while (s[i])
-	{
-		if (s[i] == c)
-			return ((char *)(s + i));
 		i++;
-	}
-	if (c == '\0')
-		return ((char *)(s + i));
-	return ((void *)(0));
+	return (i);
+}
+
+char	*ft_strtrim(char const *s1, char const *set)
+{
+	char	*res;
+	int		i;
+	int		j;
+	
+	i = 0;
+	j = ft_strlen(s1);
+	while (
+	
 }
