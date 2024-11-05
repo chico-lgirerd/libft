f3097cb015422c7c2d1e48a0cafaefccde5a4c04
Louis Girerd <lgirerd@z4r3p6.42lyon.fr>
2024-11-05 18:00:12 +0100

    updating libft

--- libft.h
+++ libft.h
@@ -6,12 +6,13 @@
 /*   By: lgirerd <lgirerd@student.42lyon.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/05 16:19:38 by lgirerd           #+#    #+#             */
-/*   Updated: 2024/11/05 16:40:11 by lgirerd          ###   ########lyon.fr   */
+/*   Updated: 2024/11/05 17:59:58 by lgirerd          ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
 
 int		ft_atoi(char *str);
 size_t	ft_strlen(const char *str);
