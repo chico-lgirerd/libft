dc7df1a79b54b02aa7c1e3d3fad278b9b460d4aa
Louis Girerd <lgirerd@z4r3p6.42lyon.fr>
2024-11-05 22:00:30 +0100

    fixed includes

--- ft_strrchr.c
+++ ft_strrchr.c
@@ -3,18 +3,22 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strrchr.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: lgirerd <marvin@42.fr>                     +#+  +:+       +#+        */
+/*   By: lgirerd <lgirerd@student.42lyon.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/04 15:30:18 by lgirerd           #+#    #+#             */
-/*   Updated: 2024/11/04 15:47:20 by lgirerd          ###   ########.fr       */
+/*   Updated: 2024/11/05 22:00:17 by lgirerd          ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include "libft.h"
 
 char	*ft_strrchr(const char *s, int c)
 {
 	int		i;
 	char	*p;
 
+	if (!s)
+		return (NULL);
 	i = 0;
 	while (s[i])
 	{
