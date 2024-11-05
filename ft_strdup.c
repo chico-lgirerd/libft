24472b621e18c4e1fdbe9c33932a1caf0df6514c
Louis Girerd <lgirerd@z4r3p6.42lyon.fr>
2024-11-05 21:12:34 +0100

    null-terminated the result in strdup

--- ft_strdup.c
+++ ft_strdup.c
@@ -6,7 +6,7 @@
 /*   By: lgirerd <lgirerd@student.42lyon.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/05 12:17:14 by lgirerd           #+#    #+#             */
-/*   Updated: 2024/11/05 20:29:18 by lgirerd          ###   ########lyon.fr   */
+/*   Updated: 2024/11/05 21:11:43 by lgirerd          ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,11 +18,12 @@ char	*ft_strdup(const char *s)
 	char	*d;
 
 	i = 0;
-	d = malloc(ft_strlen(s) * sizeof(char));
+	d = malloc((ft_strlen(s) * sizeof(char)) + 1);
 	while (i < ft_strlen(s))
 	{
 		d[i] = s[i];
 		i++;
 	}
+	d[i] = '\0';
 	return (d);
 }
