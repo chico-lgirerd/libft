9fc09d28c920eed81fbfae09f18a4974291af91d
Louis Girerd <lgirerd@z4r3p6.42lyon.fr>
2024-11-05 12:15:04 +0100

    finished strlcpy

---
+++ ft_strlcpy.c
@@ -0,0 +1,41 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcpy.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lgirerd <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/05 11:58:44 by lgirerd           #+#    #+#             */
+/*   Updated: 2024/11/05 12:10:41 by lgirerd          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+
+size_t	ft_strlen(const char *str)
+{
+	int		i;
+	size_t	size;
+
+	i = 0;
+	while (str[i])
+		i++;
+	size = sizeof(char) * i;
+	return (size);
+}
+
+size_t	ft_strlcpy(char *dst, char *src, size_t size)
+{
+	size_t	i;
+	size_t	src_len;
+
+	i = 0;
+	src_len = ft_strlen(src);
+	while (src[i] && i < size)
+	{
+		dst[i] = src[i];
+		i++;
+	}
+	dst[i] = '\0';
+	return (src_len);
+}
