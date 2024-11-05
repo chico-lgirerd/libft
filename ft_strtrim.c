b4c79c5205a5299ab4aa3a958f17d47a413964ae
Louis Girerd <lgirerd@z4r3p6.42lyon.fr>
2024-11-05 15:19:53 +0100

    finished strtrim

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -11,35 +11,20 @@
 /* ************************************************************************** */
 
 #include <stdlib.h>
+#include <stdio.h>
 
-int	ft_strstrchr(const char *s, const char *set)
+int	ft_strstrchr(const char *set, const char c)
 {
 	int	i;
-	int	j;
 
 	i = 0;
 	while (set[i])
 	{
-		j = 0;
-		while (s[i])
-		{
-			if (s[i] == c)
-				return (1);
-			j++;
-		}
+		if (c == set[i])
+			return (1);
 		i++;
 	}
 	return (0);
-}
-
-int	ft_strlen(const char *s)
-{
-	int	i;
-
-	i = 0;
-	while (s[i])
-		i++;
-	return (i);
 }
 
 char	*ft_strtrim(char const *s1, char const *set)
@@ -47,9 +32,33 @@ char	*ft_strtrim(char const *s1, char const *set)
 	char	*res;
 	int		i;
 	int		j;
-	
+	int		resi;
+
 	i = 0;
-	j = ft_strlen(s1);
-	while (
-	
+	j = 0;
+	resi = 0;
+	while (ft_strstrchr(set, s1[i]))
+		i++;
+	while (!(ft_strstrchr(set, s1[i])))
+	{	
+		j++;
+		i++;
+	}
+	i -= j;
+	res = malloc(j * sizeof(char));
+	while (!(ft_strstrchr(set, s1[i])))
+	{
+		res[resi] = s1[i];
+		resi++;
+		i++;
+	}
+	return (res);
 }
+
+/*
+int	main()
+{
+	char	*test = ft_strtrim("filsiiicoucoulisf", "fils");
+	printf("%s", test);
+	return (0);
+}*/
