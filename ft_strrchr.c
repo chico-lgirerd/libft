f7dc61a1fb6aad43f1544e62190535c6ba6f57af
Louis Girerd <lgirerd@z4r3p6.42lyon.fr>
2024-11-05 22:17:19 +0100

    fixed strncmp

--- ft_strrchr.c
+++ ft_strrchr.c
@@ -6,7 +6,7 @@
 /*   By: lgirerd <lgirerd@student.42lyon.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/04 15:30:18 by lgirerd           #+#    #+#             */
-/*   Updated: 2024/11/05 22:00:17 by lgirerd          ###   ########lyon.fr   */
+/*   Updated: 2024/11/05 22:16:29 by lgirerd          ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,14 +17,17 @@ char	*ft_strrchr(const char *s, int c)
 	int		i;
 	char	*p;
 
+	p = NULL;
 	if (!s)
 		return (NULL);
 	i = 0;
 	while (s[i])
 	{
-		if (s[i] == c)
+		if (s[i] == (char)c)
 			p = ((char *)(s + i));
 		i++;
 	}
+	if ((char)c == '\0')
+		return ((char *)(s + i));
 	return (p);
 }
