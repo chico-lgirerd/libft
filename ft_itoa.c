06a017086c3f5590af31f14c8dd35ec3027876fe
Louis Girerd <lgirerd@z4r2p6.42lyon.fr>
2024-11-06 15:04:10 +0100

    trying to fix itoa

--- ft_itoa.c
+++ ft_itoa.c
@@ -6,7 +6,7 @@
 /*   By: lgirerd <lgirerd@student.42lyon.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/05 15:36:59 by lgirerd           #+#    #+#             */
-/*   Updated: 2024/11/05 23:18:23 by lgirerd          ###   ########lyon.fr   */
+/*   Updated: 2024/11/06 15:03:39 by lgirerd          ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -41,6 +41,7 @@ char	*reverse(char *str)
 	return (str);
 }
 
+/*
 char	*ft_itoa(int nb)
 {
 	char	*res;
@@ -48,8 +49,7 @@ char	*ft_itoa(int nb)
 	int		sign;
 
 	res = malloc(sizeof(char) * 13);
-	i = 1;
-	sign = 0;
+	i = 0;
 	if (res == NULL)
 		return (NULL);
 	if (nb == -2147483648)
@@ -59,7 +59,6 @@ char	*ft_itoa(int nb)
 		sign = 1;
 		nb = -nb;
 	}
-	res[0] = '\0';
 	while (nb)
 	{
 		res[i] = (nb % 10) + '0';
@@ -68,5 +67,36 @@ char	*ft_itoa(int nb)
 	}
 	if (sign)
 		res[i] = '-';
+	return (reverse(res));
+}*/
+
+char	*ft_itoa(int nb)
+{
+	char	*res;
+	int		i;
+	int		sign;
+
+	if (nb == -2147483648)
+		return ("-2147483648");
+	res = malloc(sizeof(char) * 12);
+	if (res == NULL)
+		return (NULL);
+	i = 0;
+	sign = 0;
+	if (nb == 0)
+		return ("0");
+	if (nb < 0)
+	{
+		nb = -nb;
+		sign = 1;
+	}
+	while (nb)
+	{
+		res[i++] = (nb % 10) + '0';
+		nb /= 10;
+	}
+	if (sign)
+		res[i++] = '-';
+	res[i] = '\0';
 	return (reverse(res));
 }
