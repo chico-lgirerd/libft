5e6bb4c5988237ca20ac1f1dff7807892a79f0b3
Louis Girerd <lgirerd@z4r3p6.42lyon.fr>
2024-11-08 16:26:08 +0100

    fix : memchr

--- ft_memchr.c
+++ ft_memchr.c
@@ -6,7 +6,7 @@
 /*   By: lgirerd <lgirerd@student.42lyon.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/04 17:48:50 by lgirerd           #+#    #+#             */
-/*   Updated: 2024/11/05 18:44:43 by lgirerd          ###   ########lyon.fr   */
+/*   Updated: 2024/11/08 16:25:36 by lgirerd          ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,14 +14,16 @@
 
 void	*ft_memchr(const void *s, int c, size_t n)
 {
-	size_t	i;
-	char	*cs;
+	size_t			i;
+	char			*cs;
+	unsigned char	uc;
 
 	cs = (char *)s;
+	uc = (unsigned char)c;
 	i = 0;
 	while (i < n)
 	{
-		if (cs[i] == c)
+		if ((unsigned char)cs[i] == uc)
 			return (cs + i);
 		i++;
 	}
