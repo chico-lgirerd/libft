03d9ee29857a47be74342701491c55af6d54c4bb
Louis Girerd <louis@Mac.lan>
2024-11-09 13:14:35 +0100

    add : lstsize

--- ft_lstsize.c
+++ ft_lstsize.c
@@ -6,16 +6,23 @@
 /*   By: lgirerd <lgirerd@student.42lyon.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/08 20:43:03 by lgirerd           #+#    #+#             */
-/*   Updated: 2024/11/08 20:48:54 by lgirerd          ###   ########lyon.fr   */
+/*   Updated: 2024/11/09 13:14:10 by lgirerd          ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int ft_lstsize(t_list *lst)
+int	ft_lstsize(t_list *lst)
 {
-    int count;
-    
-    count = 1;
-    
+	int 	count;
+	t_list	*current;
+	
+	count = 0;
+	current = lst;
+	while (current != NULL)
+	{
+		count++;
+		current = current->next;
+	}
+	return (count);
 }
